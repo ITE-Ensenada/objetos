b3af73bd39b8781747f3c627620a318b9e10d862
mimisoika <noemimendozacarrillo200@gmail.com>
2023-03-09 08:43:31 -0800

    agregue un archivo de practica

---
+++ practica_clases.py
@@ -0,0 +1,28 @@
+class world:
+    10*10
+
+class personaje:
+    especie = "humanoide"
+    def _init_(self ,nombre,age,traje,nivel,tipo,magia,armas,ataque,estatus,poder):
+        self.nombre = name
+        self.age = age
+        self.traje
+        self.nivel = 7
+        self.tipo
+        self.magia
+        self.armas
+        self.ataque
+        self.estatus
+        self.poder = 6
+        def _str_(self):
+            return f"{self.nombre} hola {self.especie}"
+        def camina(self):
+            print("camina " + self.camina)
+        def ataca(self):
+            return self.ataque
+    if __name__ == '__main__':
+        nombre == input('ingresa tu nombre: ')
+        edad == input('ingresa tu edad: ')
+
+
+        

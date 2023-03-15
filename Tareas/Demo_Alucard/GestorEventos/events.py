60165d520607e6ab86be88279bc18d1bf244c34b
Grumos5 <al22760591@ite.edu.mx>
2023-03-14 19:20:55 -0700

    GestorEventos, Assets Package added | main.py, README.md edited

---
+++ Tareas/Demo_Alucard/GestorEventos/events.py
@@ -0,0 +1,34 @@
+import pygame, sys
+from pygame.locals import *
+
+def direccion(player):
+    for event in pygame.event.get():
+        print(event)
+
+        #Detecta un evento donde se oprima una tecla para realizar una accion especifica
+        if event.type == pygame.KEYDOWN:
+
+            if event.key == pygame.K_DOWN:  #Si se oprime la flecha abajo, el personaje cambia su direccion
+                player.direction = 'down'
+            
+            if event.key == pygame.K_UP:    #Si se oprime la flecha arriba, el personaje cambia su direccion
+                player.direction = 'up'
+            
+            if event.key == pygame.K_LEFT:  #Si se oprime la flecha izquierda, el personaje cambia su direccion
+                player.direction = 'left'
+            
+            if event.key == pygame.K_RIGHT: #Si se oprime la flecha derecha, el personaje cambia su direccion
+                player.direction = 'right'
+
+            #Con la letra ESCAPE se sale del juego (SE AGREGO PARA FACILIDAD DE TESTEO)
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                sys.exit() 
+
+        if event.type == QUIT:              #Si se detecta que se cierra la ventana se cierra el juego 
+            pygame.quit()
+            sys.exit()
+
+def Exit_game():
+    for event in pygame.event():
+        pass
---
+++ Tareas/Demo_Alucard/README.md
@@ -0,0 +1,9 @@
+main.py 
+Es el codigo que yo (DIO... digo osvaldo) realizo tratando de aplicar principios de POO, agregando como extra la capacidad del personaje de moverse usando las flechas del teclado
+14/03/23:
+Se distribuyo el proyecto en paquetes que ayudan a la gestion del mismo
+
+
+Problemas visibles:
+1.- el personaje puede abandonar la ventana visible por el espectador (RESUELTO 13/03/2023)
+2.- En mi computadora corre como si fuera el minecraft con shaders, extra lento 

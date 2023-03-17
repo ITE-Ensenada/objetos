9e5179331f0a0136de8fb723f4c69e65fc804bbe
MasherZN <84199151+MasherZN@users.noreply.github.com>
2023-03-16 21:03:16 -0700

    Juego poo

---
+++ Juego ed/jugador.py
@@ -0,0 +1,8 @@
+import pygame
+
+class Jugador(pygame.sprite.Sprite):
+    def __init__(self,pos):
+        super().__init__()
+        self.image = pygame.Surface((32,64))
+        self.image.fill('red')
+        self.rect = self.image.get_rect (topleft = pos)
---
+++ Juego ed/propiedades.py
@@ -0,0 +1,7 @@
+import pygame
+import sys
+from pygame.locals import *
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+pygame.init()
+

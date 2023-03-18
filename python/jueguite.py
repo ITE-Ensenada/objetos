98acbbb177898387a5253c1a14f8a1a1de8bbd65
MonsySS <127114844+MonsySS@users.noreply.github.com>
2023-03-18 00:14:28 -0700

    Update jueguite.py
    no lo había subido actualizado XD

--- python/jueguite.py
+++ python/jueguite.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, time
 from pygame.locals import*
 pygame.init()
 win=pygame.display.set_mode((740,945))
@@ -6,18 +6,37 @@ img=pygame.image.load("gatite.png")
 fondo=(255,255,255)
 pygame.init()
 FPS= 10 
-
-
-class gato:
-    especie= "cafe con leche"
-    def __init__ (Self,nombre,vida):
-        Self.nombre=nombre
-        Self.vida=250
-        Self.fuerza=10
-        Self.caminar=15
-       
 x=10
 y=98
+
+class gato:
+    especie= "cafe con leche"   
+    def especificaciones (Self,nombre,vida,fuerza,caminar):
+        Self.nombre=nombre
+        Self.vida=vida
+        Self.fuerza=fuerza
+        Self.caminar=caminar
+
+class arma_principal:
+	Espada = "1"
+	def __init__(self,material,daño,resistencia):
+		self.material=material
+		self.daño=daño
+		self.resistencias=resistencia 
+
+class arma_secundaria: 		
+	Espada = "2"
+	def __init__(self,material,daño,resistencia):
+		self.material=material
+		self.daño=daño
+		self.resistencias=resistencia 
+
+class arma_terciaria: 
+	Espada = "3"
+	def __init__(self,material,daño,resistencia):
+		self.material=material	
+		self.daño=daño
+		self.resistencia=resistencia
 
 while True: 
     for event in pygame.event.get():
@@ -41,4 +60,4 @@ while True:
     
     win.fill(fondo)
     win.blit(img,(x,y))
-    pygame.display.update()         
+    pygame.display.update()            

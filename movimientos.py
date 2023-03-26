0fbda991efc402ee282aaaa3b6aec43606251bd6
mimisoika <noemimendozacarrillo200@gmail.com>
2023-03-25 19:00:27 -0700

    image loading for the different character directions

--- movimientos.py
+++ movimientos.py
@@ -1,13 +1,9 @@
 import pygame
 from pygame.locals import *
 pygame.init()
-caminaDerecha = [pygame.image.load('imagenes/run01.png'),
-    pygame.image.load('imagenes/run02.png'),
-        pygame.image.load('imagenes/run03.png')]
+Derecha = pygame.image.load('imagenes/run01.png')
         
-caminaIzquierda = [pygame.image.load('imagenes/run01_izq.png'),
-        pygame.image.load('imagenes/run02_izq.png'),
-        pygame.image.load('imagenes/run03_izq.png')]  
+Izquierda = pygame.image.load('imagenes/run01_izq.png')
 
 Quieto = pygame.image.load('imagenes/estatico.png')  
 

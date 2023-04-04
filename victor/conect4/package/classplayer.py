2188b61f131ea7598ea99b133a70cde6d3edee4d
HEREDIAAA <andreyxks1@gmail.com>
2023-04-03 22:52:21 -0700

    wuju

---
+++ victor/conect4/package/classplayer.py
@@ -0,0 +1,7 @@
+class player :
+    def _init_(self):
+        self.nombre = ""
+        self.nombre = ""
+    def atributos(self,string):
+        self.nomre = input(string)
+        self.caracter = input("escoja un caracter para jugar( el caracter que elija aparecera en el tablero):")

ff7ceb8b0781afe7812c305c6d8d90e5afb04b12
andrespr1981 <118039027+andrespr1981@users.noreply.github.com>
2023-04-26 00:13:49 -0700

    Juego sobre naves

---
+++ Space invaders/SpaceFighters.py
@@ -0,0 +1,281 @@
+import pygame
+from pygame.locals import*
+import random
+from Meteoros import*
+
+#Fps
+reloj= pygame.time.Clock()
+fps = 60
+
+#Pantalla
+ScreenAl = 1100
+ScreenAn = 600 
+SCREEN = SCREEN = pygame.display.set_mode((ScreenAl,ScreenAn))
+
+#Nombre y icono del juego
+pygame.display.set_caption('Space Fighters')
+icono = pygame.image.load("Nave1.png")
+pygame.display.set_icon(icono)
+
+#Rgb de Colores 
+AzulCielo = (63,255,238)
+Blanco = (255, 255, 255)
+Negro = (0, 0, 0)
+Rojo = (255, 0, 0)
+Verde = (0, 255, 0)
+Azul = (0, 0, 255)
+
+#Carga de fondo
+fondo = pygame.image.load("Fondo.png")
+def dibujar_fondo():
+	SCREEN.blit(fondo,(0,0))
+
+#Clase para Nave 1 
+class Nave(pygame.sprite.Sprite):
+	def __init__(self,x,y,vida):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.transform.scale(pygame.image.load("Nave1.png"), (120, 80))
+		self.rect = self.image.get_rect()
+		self.rect.center = [x,y]
+		self.vida_inicio = vida
+		self.vida_restante = vida
+		self.ultima_bala = pygame.time.get_ticks()
+
+	def update(self):
+		VeloNave = 8
+
+		key = pygame.key.get_pressed()
+		if key[pygame.K_LEFT] and self.rect.left > 0:
+			self.rect.x -= VeloNave
+		if key[pygame.K_RIGHT] and self.rect.right < ScreenAl and self.rect.right < 500 :
+			self.rect.x += VeloNave	
+		if key[pygame.K_UP] and self.rect.y > 10 :
+			self.rect.y -= VeloNave
+		if key[pygame.K_DOWN] and self.rect.y < 480 :
+			self.rect.y += VeloNave
+		pygame.draw.rect(SCREEN,Rojo,(self.rect.x,(self.rect.bottom + 10),self.rect.width,15))
+		if self.vida_restante > 0:
+			pygame.draw.rect(SCREEN,Verde,(self.rect.x,(self.rect.bottom + 10),int(self.rect.width * (self.vida_restante / self.vida_inicio)),15))
+		if self.vida_restante == 0:
+			self.kill()
+
+		enfriamiento = 500
+		tiempo = pygame.time.get_ticks()
+		if key[pygame.K_m] and tiempo - self.ultima_bala > enfriamiento:
+			bala = Balas (self.rect.centerx + 80, self.rect.centery)
+			Balas_group.add(bala)
+			self.ultima_bala = tiempo
+
+		Ya=250
+		Frio = 3000
+		Limite1 = 10000
+		Limite2 = 8000
+		Limite3 = 10000
+		tiempo = pygame.time.get_ticks()
+		if tiempo < Limite1:
+			if tiempo > Limite2/2:
+				pygame.draw.rect(SCREEN,AzulCielo,(10,10,40,40))
+				if tiempo > Limite2:
+					pygame.draw.rect(SCREEN,Rojo,(20,20,20,20))
+					if key[pygame.K_n] and tiempo - self.ultima_bala > Ya:
+						Ultis = Ulti(self.rect.centerx + 80, self.rect.centery)
+						Balas_group.add(Ultis)
+						self.ultima_bala = tiempo
+				if tiempo > Limite2:
+					pygame.draw.rect(SCREEN,Rojo,(10,10,40,40))
+
+#Creacion para agrupar las naves
+Naves_group = pygame.sprite.Group()
+Nave = Nave(int(70), 310, 5)
+Naves_group.add(Nave)
+
+#Clase para Nave 2
+class Nave2(pygame.sprite.Sprite):
+	def __init__(self,x,y, vida):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.transform.scale(pygame.image.load("Nave2.png"), (120, 80))
+		self.rect = self.image.get_rect()
+		self.rect.center = [x,y]
+		self.vida_inicio = vida
+		self.vida_restante = vida
+		self.ultima_bala = pygame.time.get_ticks()
+
+	def update(self):
+		VeloNave = 8
+
+		key = pygame.key.get_pressed()
+		if key[pygame.K_a] and self.rect.left > 0 and self.rect.left > 580:
+			self.rect.x -= VeloNave
+		if key[pygame.K_d] and self.rect.right < ScreenAl:
+			self.rect.x += VeloNave	
+		if key[pygame.K_w] and self.rect.y > 10 :
+			self.rect.y -= VeloNave
+		if key[pygame.K_s] and self.rect.y < 480 :
+			self.rect.y += VeloNave
+		pygame.draw.rect(SCREEN,Rojo,(self.rect.x,(self.rect.bottom + 10),self.rect.width,15))
+		if self.vida_restante >= 1:
+			wow = pygame.draw.rect(SCREEN,Verde,(self.rect.x,(self.rect.bottom + 10),int(self.rect.width * (self.vida_restante / self.vida_inicio)),15))
+		if self.vida_restante == 0:
+			self.kill()
+
+		enfriamiento = 500
+		tiempo = pygame.time.get_ticks()
+		if key[pygame.K_f] and tiempo - self.ultima_bala > enfriamiento:
+			bala = Balas2 (self.rect.x - 40, self.rect.centery)
+			Balas_group.add(bala)
+			self.ultima_bala = tiempo 
+
+		Ya=250
+		Frio = 3000
+		Limite1 = 10000
+		Limite2 = 8000
+		Limite3 = 10000
+		tiempo = pygame.time.get_ticks()
+		if tiempo < Limite1:
+			if tiempo > Limite2/2:
+				pygame.draw.rect(SCREEN,AzulCielo,(1050,10,40,40))
+				if tiempo > Limite2:
+					pygame.draw.rect(SCREEN,Rojo,(1050,20,20,20))
+					if key[pygame.K_e] and tiempo - self.ultima_bala > Ya:
+						Ultis = Ulti2(self.rect.centerx - 60, self.rect.centery)
+						Balas_group.add(Ultis)
+						self.ultima_bala = tiempo
+					if tiempo > Limite2:
+						pygame.draw.rect(SCREEN,Rojo,(1050,10,40,40))
+
+#Creacion de segundo grupo para agrupar naves
+Naves_group2 = pygame.sprite.Group()
+Nave2 = Nave2(int(1030), 310, 5)
+Naves_group2.add(Nave2)
+
+#Clase para balas 
+class Balas(pygame.sprite.Sprite):
+	def __init__(self,x,y):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.transform.scale(pygame.image.load("Bala.png"), (50,20))
+		self.rect = self.image.get_rect()
+		self.rect.center = [x,y]
+
+	def update(self):
+		self.rect.x += 5
+		if self.rect.bottom < 0:
+			self.kill()
+		if pygame.sprite.spritecollide(self, Naves_group2, False):
+			self.kill()
+			Nave2.vida_restante -= 1
+		if pygame.sprite.spritecollide(self, Meteoro_group, True):
+			self.kill()
+
+class Balas2(pygame.sprite.Sprite):
+	def __init__(self,x,y):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.transform.scale(pygame.image.load("Bala2.png"), (50,20))
+		self.rect = self.image.get_rect()
+		self.rect.center = [x,y]
+
+	def update(self):
+		self.rect.x -= 5
+		if self.rect.bottom < 0:
+			self.kill()
+		if pygame.sprite.spritecollide(self, Naves_group, False):
+			self.kill()
+			Nave.vida_restante -= 1
+		if pygame.sprite.spritecollide(self, Meteoro_group, True):
+			self.kill()
+
+#Creacion para agrupar las balas
+Balas_group = pygame.sprite.Group()
+
+#Creacion de clase para la ulti
+class Ulti(pygame.sprite.Sprite):
+	def __init__(self,x,y):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.transform.scale(pygame.image.load("Bala.png"), (50,20))
+		self.rect = self.image.get_rect()
+		self.rect.center = [x,y]
+
+	def update(self):
+		self.rect.x += 5
+		if self.rect.bottom < 0:
+			self.kill()
+		if pygame.sprite.spritecollide(self, Naves_group2, False):
+			self.kill()
+			Nave2.vida_restante -= 1
+		if pygame.sprite.spritecollide(self, Meteoro_group, True):
+			self.kill()
+
+class Ulti2(pygame.sprite.Sprite):
+	def __init__(self,x,y):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.transform.scale(pygame.image.load("Bala2.png"), (50,20))
+		self.rect = self.image.get_rect()
+		self.rect.center = [x,y]
+
+	def update(self):
+		self.rect.x -= 5
+		if self.rect.bottom < 0:
+			self.kill()
+		if pygame.sprite.spritecollide(self, Naves_group, False):
+			self.kill()
+			Nave.vida_restante -= 1
+		if pygame.sprite.spritecollide(self, Meteoro_group, True):
+			self.kill()
+
+#Creacion de agrupacion de las ulti
+Ultis_group = pygame.sprite.Group()
+
+
+#Codigo para hacer que cada meteoro aparezca en pantalla
+Meteoro_group = pygame.sprite.Group()
+Meteoro1 = Meteoro1(int(random.randint(500,600)),random.randint(0,590))
+Meteoro2 = Meteoro2(int(random.randint(500,600)),random.randint(0,590))
+Meteoro3 = Meteoro3(int(random.randint(500,600)),random.randint(0,590))
+Meteoro4 = Meteoro4(int(random.randint(500,600)),random.randint(0,590))
+Meteoro5 = Meteoro5(int(random.randint(500,600)),random.randint(0,590))
+Meteoro6 = Meteoro6(int(random.randint(500,600)),random.randint(0,590))
+Meteoro7 = Meteoro7(int(random.randint(500,600)),random.randint(0,590))
+Meteoro8 = Meteoro8(int(random.randint(500,600)),random.randint(0,590))
+Meteoro9 = Meteoro9(int(random.randint(500,600)),random.randint(0,590))
+Meteoro10 = Meteoro10(int(random.randint(500,600)),random.randint(0,590))
+Meteoro_group.add(Meteoro1)
+Meteoro_group.add(Meteoro2)
+Meteoro_group.add(Meteoro3)
+Meteoro_group.add(Meteoro4)
+Meteoro_group.add(Meteoro5)
+Meteoro_group.add(Meteoro6)
+Meteoro_group.add(Meteoro7)
+Meteoro_group.add(Meteoro8)
+Meteoro_group.add(Meteoro9)
+Meteoro_group.add(Meteoro10)
+
+#While del juego
+Activo = True
+while Activo:
+
+	reloj.tick(fps)
+	dibujar_fondo()
+
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
+
+	#Actualizacion en pantalla de todo
+	Meteoro1.update()		
+	Meteoro2.update()
+	Meteoro3.update()		
+	Meteoro4.update()
+	Meteoro5.update()		
+	Meteoro6.update()
+	Meteoro7.update()
+	Meteoro8.update()
+	Meteoro9.update()
+	Meteoro10.update()	
+	Balas_group.update()
+	Nave.update()
+	Nave2.update()
+	Meteoro_group.draw(SCREEN)	
+	Balas_group.draw(SCREEN)
+	Naves_group.draw(SCREEN)
+	Naves_group2.draw(SCREEN)	
+	pygame.display.update()

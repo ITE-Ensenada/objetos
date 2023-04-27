50ad5a4b02a2e1224d7973948bdda3d7ce060f40
mimisoika <noemimendozacarrillo200@gmail.com>
2023-04-27 07:17:24 -0700

    ayudenme yano se que hacer

--- movimientos.py
+++ movimientos.py
@@ -1,10 +1,7 @@
 import pygame
 from pygame.locals import *
 pygame.init()
-Derecha = pygame.image.load('imagenes/run01.png')
-        
+Derecha = pygame.image.load('imagenes/run01.png')       
 Izquierda = pygame.image.load('imagenes/run01_izq.png')
-
 Quieto = pygame.image.load('imagenes/estatico.png')  
-
 Salto = pygame.image.load('imagenes/salto.png')

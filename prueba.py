d1f066664d7f88859e690a918f0c3d836483a97b
JulioGorosave <127075895+JulioGorosave@users.noreply.github.com>
2023-04-30 10:45:05 -0700

    Cambios en mi juego

--- prueba.py
+++ prueba.py
@@ -1,48 +1,78 @@
-import pygame, sys
+import pygame, sys, time, random
 from pygame.locals import *
 
+#COSO PARA DELIMITAR LAS DIMENSIONES DE LA PANTALLA
 pygame.init()
+width = 500
+height = 700
+play_surface = pygame.display.set_mode((width, height))
+fps = pygame.time.Clock()
 
-FPS = 10 # frames per second setting
+#FUNCION PARA GENERAR DE MANERA ALEATORIA LOS TUBOS
+def pipe_random_height():
+    pipe_h = [random.randint(200, (height/2)-20), random.randint((height/2)+20, height-200)]
+    return pipe_h
 
-fpsClock = pygame.time.Clock()
+#AQUI SE VAN A DEFINIR TODAS LA VARIABLES PARA PODER JUGAR, GRAVEDAD, VELOCIDAD, ETC.
+def main():
+    score = 0
+    player_pos = [100, 350]
+    gravity = 1
+    speed = 0
+    jump = -30
 
-# set up the window
+    pipe_pos = 700
+    pipe_widht = 50
+    pipe_height = pipe_random_height()
 
-SCREEN = pygame.display.set_mode((1200, 800), 0, 32)
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
 
-pygame.display.set_caption('Animation')
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    speed += jump
 
-WHITE = (179, 113, 209)
 
-personajeImg = pygame.image.load('zero.png')
-personajeImg = pygame.transform.scale(personajeImg, (100, 200))
+        speed += gravity
+        speed *= 0.95
+        player_pos[1] += speed
+        
+        #GENERACION DE TUBOS CUANDO PASAS UNO
+        if pipe_pos >= -20:
+            pipe_pos -= 10
+        else:
+            pipe_pos = 700
+            pipe_height = pipe_random_height()
+            score += 1
 
-personajeX = 10
+        #PANTALLA
+        play_surface.fill((0,0,0))
 
-personajeY = 10
+        #AQUI SE VA ESTAR DIBUJANDO EL JUGADOR (DEBERIA SER UN PAJARO PERO LUEGO LO CAMBIO XD)
+        pygame.draw.circle(play_surface, (255, 160, 60), (int(player_pos[0]), int(player_pos[1])), 20)
 
-while True: # the main game loop
+        #ESTA COSA ES PARA DIBUJAR LOS "TUBOS "XD
+        pygame.draw.rect(play_surface, (200,200,200), [pipe_pos, 0, pipe_widht, pipe_height[0]], 0)
+        pygame.draw.rect(play_surface, (200,200,200), [pipe_pos, pipe_height[1], pipe_widht, 500], 0)
 
-	
-    SCREEN.fill(WHITE)
-    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                personajeY -= 20
-            if event.key == pygame.K_DOWN:
-                personajeY += 20
-            if event.key == pygame.K_LEFT:
-                personajeX -= 20
-            if event.key == pygame.K_RIGHT:
-                personajeX += 20
-	
-	
-    SCREEN.blit(personajeImg, (personajeX, personajeY))
-                
-    pygame.display.update()
-    fpsClock.tick(FPS)
+        #COSO PARA LAS COLISIONES
+        if player_pos[1] <= pipe_height[0] or player_pos[1] >= pipe_height[1]:
+            if player_pos[0] in list(range(pipe_pos, pipe_pos+pipe_widht)):
+                print(f"Game Over. Score {score})")
+
+        if player_pos[1] >= height:
+            player_pos[1] = height
+            speed = 0
+        elif player_pos[1] <= 0:
+            player_pos[1] = 0
+            speed = 0
+
+        pygame.display.flip()
+        fps.tick(30)
+
+
+main()
+pygame.quit()

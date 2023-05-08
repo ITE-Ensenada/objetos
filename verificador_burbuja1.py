d10b627b9d5ef96ed4d8d8a7f188b91e2f24480f
pako <pako@ite.edu.mx>
2023-05-08 08:51:20 -0700

    Ahita shava

--- verificador_burbuja1.py
+++ verificador_burbuja1.py
@@ -19,29 +19,21 @@ def verificador_burbuja(arreglo):
         que al hacer un recorrido, el primer
         elemento sea menor al que le sigue
     '''
-    ordenado = True
     n_arreglo = len(arreglo)
-
     for i in range(n_arreglo-1):
-	    #j = i + 1
         for j in range(i+1,n_arreglo):
-            # Imprimimos para saber las posiciones en las que nos encontramos
-            """
-            print("Index I: ",i, "\t\t\t\tIndex J: ",j)
-            print("value I: ", arreglo[i], "\t\t\t\tvalue J: ", arreglo[j])
-            """
-            # validacion de orden
+            # si encuentra un error
             if arreglo[i] > arreglo[j]:
-                ordenado = False
-    if ordenado is True:
-        print("Tu arreglo esta ORDENADO")
-    else:
-        print("Tu arreglo esta DESORDENADO")
-
+                return False
+    return True
+#elementos = [0,1,2,3,5,6,8,7]
 elementos = [3, 1, 8, 19, 14,32,1,2,786,0,-1,4,7,2,3,23,45,54,62,78,90,-0,-21,3, 1, 8, 19, 14,32,1,2,786,0,-1,4,7,2,3,23,45,54,62,3, 1, 8, 19, 14,32,1,2,786,0,-1,4,7,2,3,23,45,54,62,3, 1, 8, 19, 14,32,1,2,786,0,-1,4,7,2,3,23,45,54,62,3, 1, 8, 19, 14,32,1,2,786,0,-1,4,7,2,3,23,45,54,62,3, 1, 8, 19, 14,32,1,2,786,0,-1,4,7,2,3,23,45,54,62,3, 1, 8, 19, 14,32,1,2,786,0,-1,4,7,2,3,23,45,54,62,3, 1, 8, 19, 14,32,1,2,786,0,-1,4,7,2,3,23,45,54,62,3, 1, 8, 19, 14,32,1,2,786,0,-1,4,7,2,3,23,45,54,62,3, 1, 8, 19, 14,32,1,2,786,0,-1,4,7,2,3,23,45,54,62,3, 1, 8, 19, 14,32,1,2,786,0,-1,4,7,2,3,23,45,54,62,3, 1, 8, 19, 14,32,1,2,786,0,-1,4,7,2,3,23,45,54,62,3, 1, 8, 19, 14,32,1,2,786,0,-1,4,7,2,3,23,45,54,62,3, 1, 8, 19, 14,32,1,2,786,0,-1,4,7,2,3,23,45,54,62,3, 1, 8, 19, 14,32,1,2,786,0,-1,4,7,2,3,23,45,54,62,3, 1, 8, 19, 14,32,1,2,786,0,-1,4,7,2,3,23,45,54,62,3, 1, 8, 19, 14,32,1,2,786,0,-1,4,7,2,3,23,45,54,62,3, 1, 8, 19, 14,32,1,2,786,0,-1,4,7,2,3,23,45,54,62]
 
-verificador_burbuja(elementos)
-
+verifica = verificador_burbuja(elementos)
+if verifica:
+    print("Todo bien")
+else: 
+    print("Todo mal")
 # Medir tiempo
 end_time = time.time()
 total_time = end_time - start_time

cedf7444d608fe253ba9739c45acb567bf3c2fe4
CARRILLO07 <js04carrillo@gmail.com>
2023-05-11 08:22:14 -0700

    verificador

--- verificadores/verificadorburbuja.py
+++ verificadores/verificadorburbuja.py
@@ -1,13 +1,28 @@
 '''Algoritmo de ordenamiento de numeros'''
-def ordenamiento_burbuja(lista):
-    '''Este es un verificador que hace un ordenamiento de los numeros de menor a mayor.'''
+def ordenamiento_burbuja_con_verificador(lista):
+    '''encargado del ordenamiento de los numeros'''
     n = len(lista)
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if lista[j] > lista[j+1]:
-                lista[j], lista[j+1] = lista[j+1], lista[j]
+    ordenado = False
+    while not ordenado:
+        ordenado = True
+        for i in range(n-1):
+            if lista[i] > lista[i+1]:
+                lista[i], lista[i+1] = lista[i+1], lista[i]
+                ordenado = False
     return lista
-numeros = [10, 5, 8, 3, 2, 6, 14, 141, 145, 123, 9, 11, 13]
-numeros_ordenados = ordenamiento_burbuja(numeros)
-print(numeros_ordenados) # [2, 3, 5, 6, 8, 10]
 
+def verificar_orden(lista):
+    '''Se encarga de verificar si los numeros estan ordenados'''
+    n = len(lista)
+    for i in range(n-1):
+        if lista[i] > lista[i+1]:
+            return False
+    return True
+#numeros = [10, 5, 8, 3, 2, 6]
+numeros = [1, 2, 3, 10, 30, 50, 11, 17, 20]
+
+if verificar_orden(numeros):
+    print("La lista ya está ordenada.")
+else:
+    numeros_ordenados = ordenamiento_burbuja_con_verificador(numeros)
+    print("La lista ordenada:", numeros_ordenados)

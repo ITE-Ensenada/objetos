8d274e4e802f19e82326bb83c4050ca5d4a70cce
Salva007 <103088037+slv14@users.noreply.github.com>
2023-05-12 16:04:33 -0800

    funciones para burbuja

---
+++ algoritmo_actualizacion.py
@@ -0,0 +1,20 @@
+'''
+Este modulo se encarga de actualizar
+un arreglo:
+
+1. Agrega un numero
+2. Elimina un numero
+3. Localiza un numero
+
+'''
+
+def add_number(array):
+    '''
+    Funcion agregar numero al arreglo, 
+    esto anexando el numero en la posicion 
+    que le corresponde en orden
+    '''
+    numero = int(input("Ingrese un numero"))
+    for i in range(len(array)):
+        
+    

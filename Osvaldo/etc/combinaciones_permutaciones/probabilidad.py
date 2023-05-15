b133c159fed90115c9c1b7dff6edf2671ae904ef
Grum5 <al22760591@ite.edu.mx>
2023-05-15 03:25:22 -0700

    si

--- Osvaldo/etc/combinaciones_permutaciones/probabilidad.py
+++ Osvaldo/etc/combinaciones_permutaciones/probabilidad.py
@@ -1,3 +1,39 @@
-arreglo = open("archivo.txt", "r").read().split("\n")
 
-print(arreglo)
+def factorial(valor):
+
+    for num in range(1, valor):
+        valor *= num
+
+    return valor
+
+
+
+def convertor_txt_py():
+    with open('archivo.txt', 'r') as archivo:
+        contenido = archivo.read()
+
+    numeros_str = contenido.split(',')
+
+    arreglo = [int(numero) for numero in numeros_str]
+
+    return arreglo
+
+
+def combinaciones(arreglo):
+    
+    longitud = len(arreglo) # Longitud del arreglo
+    print( range(longitud) )
+
+
+def permutaciones(arreglo):
+    pass
+
+
+def permutaciones_con_repeticion(arreglo):
+    pass
+
+
+
+
+if __name__ == '__main__':
+    combinaciones(arreglo = convertor_txt_py())

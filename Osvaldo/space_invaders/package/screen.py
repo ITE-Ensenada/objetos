7d989c0a658f5fccf264daf53ede8979dda981dd
Grum5 <al22760591@ite.edu.mx>
2023-05-15 20:20:58 -0700

    Agregados nuevos metodos

--- Osvaldo/space_invaders/package/screen.py
+++ Osvaldo/space_invaders/package/screen.py
@@ -15,10 +15,10 @@ class Screen:
 
         self.delta_time = 1 # Variable para el delta time
 
-        self.background = self.create_background() # Crear fondo
+        self.background = self.create_background_game() # Crear fondo
 
 
-    def create_background(self):
+    def create_background_game(self):
         '''Metodo que se encarga de crear el fondo del juego'''
 
         background = pygame.image.load('Assets/Stars_background.jpg').convert_alpha()
@@ -31,3 +31,13 @@ class Screen:
             '''Dibujar fondo'''''
     
             self.screen.blit(self.background, (0, 0))
+    
+    def draw_menu(self):
+        font = pygame.font.Font(None, 36)
+        title_text = font.render("Juego Espacial", True, (255, 255, 255))
+        start_text = font.render("Presiona ESPACIO para comenzar", True, (255, 255, 255))
+
+        self.screen.blit(title_text, (300, 200))
+        self.screen.blit(start_text, (250, 300))
+        
+        pygame.display.update()
